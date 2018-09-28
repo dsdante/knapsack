67660c1276170da0ce050a991d5db2f8df67357b
Dante <dsdante@protonmail.com>
2018-09-28 21:36:20 +0300

    Fixed format

--- knapsack-naive.c
+++ knapsack-naive.c
@@ -6,9 +6,9 @@
 int main(int argc, char *argv[])
 {
     char *filename = "input"; // may be overriden with an argument
-	double limit;
-	int n = 0;
-	double items[64];
+    double limit;
+    int n = 0;
+    double items[64];
 
     if (argc >= 2)
         filename = argv[1];
@@ -27,17 +27,17 @@ int main(int argc, char *argv[])
     double sum_best = 0;
     long mask_best = 0x0;
     for (long mask = 0; mask < 0x1<<(n); mask++) {
-    	double sum = 0;
-    	for (int i = 0; i < n; i++)
-    		if ((mask >> i) & 0x1)
-    			sum += items[i];
-    	if (sum_best < sum && sum <= limit) {
-    		sum_best = sum;
-    		mask_best = mask;
-    	}
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+            if ((mask >> i) & 0x1)
+                sum += items[i];
+        if (sum_best < sum && sum <= limit) {
+            sum_best = sum;
+            mask_best = mask;
+        }
     }
 
-	struct timespec time_end;
+    struct timespec time_end;
     clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);
     double time = 1e-9 * (time_end.tv_nsec - time_start.tv_nsec) + (time_end.tv_sec - time_start.tv_sec);
     printf("time: %.3f s\n", time);
